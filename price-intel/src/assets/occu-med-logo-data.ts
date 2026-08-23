2602714d6a0e4a3c6d2c710ccdf4b7a8bb63e29c
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-23 13:23:32 -0700

    Use the shared Occu-Med wordmark asset

---
+++ price-intel/src/assets/occu-med-logo-data.ts
@@ -0,0 +1,3 @@
+const occuMedLogoDataUrl = "https://base44.app/api/apps/69f08d58f2a02a258d395477/files/mp/public/69f08d58f2a02a258d395477/ac708dd5f_logo-transparent.png";
+
+export default occuMedLogoDataUrl;
