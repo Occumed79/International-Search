e0b12a3528eeb8be6e17bd557e4530cada8cb9be
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-23 15:49:57 -0700

    Load integrated Command Center visual polish

--- price-intel/src/main.tsx
+++ price-intel/src/main.tsx
@@ -4,5 +4,6 @@ import "./index.css";
 import "./palette.css";
 import "./palette-effects.css";
 import "./ux-fixes.css";
+import "./command-center-polish.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
